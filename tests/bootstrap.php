d8ac7a774dfe1af7070a6111e7967be506c58bcd
Milan Pála <milan.pala@peckadesign.cz>
2018-08-15 16:07:21 +0200

    Upraven autoloading pro PHPStan

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,5 @@
 <?php declare(strict_types = 1);
 
-function createContainer(Nette\DI\Compiler $source, array $config = []): Nette\DI\Container
-{
-	$class = 'Container' . \md5((string) \lcg_value());
-	$code = $source->addConfig($config)->setClassName($class)->compile();
-	eval($code);
-
-	return new $class;
-}
-
 return \call_user_func(function () {
 	require_once __DIR__ . '/../vendor/autoload.php';
 
